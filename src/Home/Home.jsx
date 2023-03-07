7c9051f6f1d00c22617d43c4fdaf52d6e892e468
Not Pro-At-Grammer <85447436+ayush475@users.noreply.github.com>
2023-03-07 15:07:54 +0545

    black

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -34,19 +34,19 @@ Kathmandu, Nepal</p>
 </Link>
 </div>
 <div className='special'>
-    <h1> Special Thanks to</h1>
+    <h1 className='test'> Special Thanks to</h1>
     <span>Our supervisor</span>
     <p className='santa'> Er.Santa Maharjan</p>
     {/* <span>And </span> */}
     <br/>
-    <span>External Examiner</span>
+    <span className='test'>External Examiner</span>
     <p className='santa'> Dr.Ganesh Gautam</p>
     {/* <span>And </span> */}
     <br/>
-    <span>Internal Examiner</span>
+    <span className='test'>Internal Examiner</span>
     <p className='santa'> Er.Saroj Shakya</p>
     <span>And</span><br/>
-    <span> for providing us  with hardware components and help</span>
+    <span className='test'> for providing us  with hardware components and help</span>
     <Link to='https://www.facebook.com/techkeynp/'>
     <p id='madhu'> TechKey Electronics --Sanepa</p>
     </Link>
